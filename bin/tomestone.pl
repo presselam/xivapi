2193772e275bd2095d1b48228d84909a3d3507eb
Andrew M Pressel <ufotofu@whistlinglemons.com>
2020-02-23 18:22:26 -0500

    external config

--- bin/tomestone.pl
+++ bin/tomestone.pl
@@ -7,7 +7,7 @@ use autodie;
 use Cwd qw( abs_path );
 use File::Basename;
 use Getopt::Long;
-use JSON;
+use YAML::Tiny;
 use LWP;
 
 use Toolkit;
@@ -20,12 +20,17 @@ BEGIN {
     if( !-d $ENV{'CACHE_DIR'} ) {
         mkdir( $ENV{'CACHE_DIR'} );
     }
+    $ENV{'CONF_DIR'} = "$ENV{'APP_DIR'}/etc";
+    if( !-d $ENV{'CONF_DIR'} ) {
+        mkdir( $ENV{'CONF_DIR'} );
+    }
 }
 use lib $ENV{'LIB_DIR'};
 use XIVAPI;
 
 my %opts = (
     'lodestone' => 10001355,
+    'conf'      => "$ENV{'CONF_DIR'}/tomestone.yml",
     'cache'     => 1
 );
 if( !GetOptions(
@@ -34,52 +39,6 @@ if( !GetOptions(
     ) {
     die("Invalid incantation\n");
 }
-
-my %config = (
-    'RDM' =>
-        { stats => ['Intelligence'], shops => [ 1769972, 1769975, 1770052 ] },
-    'BLM' =>
-        { stats => ['Intelligence'], shops => [ 1769972, 1769975, 1770052 ] },
-    'SMN' =>
-        { stats => ['Intelligence'], shops => [ 1769972, 1769975, 1770052 ] },
-
-    'BRD' =>
-        { stats => ['Dexterity'], shops => [ 1769971, 1769974, 1770051 ] },
-    'MCH' =>
-        { stats => ['Dexterity'], shops => [ 1769971, 1769974, 1770051 ] },
-    'DNC' =>
-        { stats => ['Dexterity'], shops => [ 1769971, 1769974, 1770051 ] },
-
-    'MNK' => {
-        stats => [ 'Strength', 'Critical Hit' ],
-        shops => [ 1769971,    1769974, 1770051 ]
-    },
-    'DRG' => {
-        stats => [ 'Strength', 'Critical Hit' ],
-        shops => [ 1769971,    1769974, 1770051 ]
-    },
-    'NIN' => {
-        stats => [ 'Dexterity', 'Critical Hit' ],
-        shops => [ 1769971,     1769974, 1770051 ]
-    },
-    'SAM' => {
-        stats => [ 'Strength', 'Critical Hit' ],
-        shops => [ 1769971,    1769974, 1770051 ]
-    },
-
-    'PLD' =>
-        { stats => ['Vitality','Tenacity'], shops => [ 1769971, 1769974, 1770051 ] },
-    'WAR' =>
-        { stats => ['Vitality','Tenacity'], shops => [ 1769971, 1769974, 1770051 ] },
-    'DRK' =>
-        { stats => ['Vitality','Tenacity'], shops => [ 1769971, 1769974, 1770051 ] },
-    'GNB' =>
-        { stats => ['Vitality','Tenacity'], shops => [ 1769971, 1769974, 1770051 ] },
-
-    'MIN' => { stats => ['Gathering'], shops => [1769991] },
-    'LTW' => { stats => [ 'Craftsmanship', 'Control' ], shops => [1769990] },
-    'GSM' => { stats => [ 'Craftsmanship', 'Control' ], shops => [1769990] },
-);
 
 main();
 exit(0);
@@ -93,127 +52,145 @@ sub main {
         verbose  => $opts{'verbose'},
     );
 
+    my %config;
+    if( -f $opts{'conf'} ) {
+        my $yml = YAML::Tiny->read( $opts{'conf'} );
+        %config = %{ shift( @{$yml} ) };
+    } else {
+        message('Unable to open config file');
+        return;
+    }
+
     my $obj = getCharacterSheet( $xivapi, \%config );
 
-    message(
-        "Current Class: '$obj->{'Character'}{'ActiveClassJob'}{'Job'}{'Name'}'"
-    );
-
+    my $name = $obj->{'Character'}{'ActiveClassJob'}{'Job'}{'Name'};
     my $job  = $obj->{'Character'}{'ActiveClassJob'}{'Job'}{'Abbreviation'};
+    my $lvl  = $obj->{'Character'}{'ActiveClassJob'}{'Level'};
     my $gear = $obj->{'Character'}{'GearSet'}{'Gear'};
 
-    foreach my $shop ( @{ $config{$job}{'shops'} } ) {
-        $obj = $xivapi->specialshop($shop);
+    message("Current Class: '$job -- $name -- $lvl'");
 
-        my %shop;
-        my %slotMap;
-        foreach my $key ( sort keys %{$obj} ) {
-            next unless( $key =~ /([a-zA-Z]+)(\d+)$/ );
-            my $name       = $1;
-            my $internalId = $2;
+    my $conf  = $config{$job};
+    my $lower = 1;
+    foreach my $shopLevel ( sort keys %{ $conf->{'shops'} } ) {
+        next unless( $lvl > $lower && $lvl <= $shopLevel );
 
-            if( $name eq 'ItemCost' ) {
-                $shop{$internalId}{$name} = $obj->{$key}{'Name'};
-            }
+        foreach my $shop ( @{ $conf->{'shops'}{$shopLevel} } ) {
+            $obj = $xivapi->specialshop($shop);
 
-            if( $name eq 'CountCost' ) {
-                $shop{$internalId}{$name} = $obj->{$key};
-            }
+            my %shop;
+            my %slotMap;
+            foreach my $key ( sort keys %{$obj} ) {
+                next unless( $key =~ /([a-zA-Z]+)(\d+)$/ );
+                my $name       = $1;
+                my $internalId = $2;
 
-            if( $name eq 'ItemReceive' ) {
-                my $itemId = $obj->{$key}{'ID'};
-                if( defined($itemId) ) {
+                if( $name eq 'ItemCost' ) {
                     $shop{$internalId}{$name} = $obj->{$key}{'Name'};
-                    $shop{$internalId}{'ItemId'} = $itemId;
-                    $shop{$internalId}{'ItemSlot'}
-                        = $obj->{$key}{'EquipSlotCategoryTargetID'};
-                    my $item = $xivapi->item($itemId);
-                    $shop{$internalId}{'ItemStats'} = $item->{'Stats'};
+                }
 
-                    if( $item->{'ClassJobCategory'}{$job} == 1 ) {
-                        $slotMap{ $obj->{$key}{'EquipSlotCategoryTargetID'} }
-                            = $internalId;
+                if( $name eq 'CountCost' ) {
+                    $shop{$internalId}{$name} = $obj->{$key};
+                }
+
+                if( $name eq 'ItemReceive' ) {
+                    my $itemId = $obj->{$key}{'ID'};
+                    if( defined($itemId) ) {
+                        $shop{$internalId}{$name} = $obj->{$key}{'Name'};
+                        $shop{$internalId}{'ItemId'} = $itemId;
+                        $shop{$internalId}{'ItemSlot'}
+                            = $obj->{$key}{'EquipSlotCategoryTargetID'};
+                        my $item = $xivapi->item($itemId);
+                        $shop{$internalId}{'ItemStats'} = $item->{'Stats'};
+
+                        if( $item->{'ClassJobCategory'}{$job} == 1 ) {
+                            $slotMap{ $obj->{$key}
+                                    {'EquipSlotCategoryTargetID'} }
+                                = $internalId;
+                        }
                     }
                 }
             }
-        }
 
-        my @table;
-        foreach my $key ( keys %{$gear} ) {
-            next if( $key eq 'SoulCrystal' );
-            my $itemRef = $gear->{$key}{'Item'};
-            my $itemId  = $itemRef->{'ID'};
-            my $rarity  = $itemRef->{'Rarity'};
-            #            printObject($itemRef);
+            my @table;
+            foreach my $key ( keys %{$gear} ) {
+                next if( $key eq 'SoulCrystal' );
+                my $itemRef = $gear->{$key}{'Item'};
+                my $itemId  = $itemRef->{'ID'};
+                my $rarity  = $itemRef->{'Rarity'};
+                #            printObject($itemRef);
 
-            my @row = ( $key, $itemId, $itemRef->{'Name'} );
+                my @row = ( $key, $itemId, $itemRef->{'Name'} );
 
-            my $item = $xivapi->item($itemId);
-            my $slot = $item->{'EquipSlotCategoryTargetID'};
+                my $item = $xivapi->item($itemId);
+                my $slot = $item->{'EquipSlotCategoryTargetID'};
 
-            next unless exists( $slotMap{$slot} );
+                next unless exists( $slotMap{$slot} );
 
-            my $piece    = $shop{ $slotMap{$slot} };
-            my $useValue = undef;
-            my $useStat  = undef;
-            foreach my $stat ( @{ $config{$job}{stats} } ) {
-                if( !defined($useValue) ) {
-                    $useStat = $stat;
-                    if( $rarity == 1 ) {
-                        $useValue = $item->{'Stats'}{$stat}{'HQ'};
-                    } else {
-                        $useValue = $item->{'Stats'}{$stat}{'NQ'};
+                my $piece    = $shop{ $slotMap{$slot} };
+                my $useValue = undef;
+                my $useStat  = undef;
+                foreach my $stat ( @{ $config{$job}{stats} } ) {
+                    if( !defined($useValue) ) {
+                        $useStat = $stat;
+                        if( $rarity == 1 ) {
+                            $useValue = $item->{'Stats'}{$stat}{'HQ'};
+                        } else {
+                            $useValue = $item->{'Stats'}{$stat}{'NQ'};
+                        }
                     }
+
                 }
+                my $target = $piece->{'ItemStats'}{$useStat}{'NQ'};
+                my $delta  = $target - $useValue;
+                my $rate
+                    = $delta != 0
+                    ? sprintf( '%.02f', $piece->{'CountCost'} / $delta )
+                    : 0;
 
+                push( @row, $useValue, $delta, $rate );
+
+                push( @row, $piece->{'ItemReceive'}, $piece->{'CountCost'} );
+                #            foreach my $stat ( @{ $config{$job}{stats} } ) {
+                my $value = $piece->{'ItemStats'}{$useStat}{'NQ'};
+                push( @row, $value );
+                #            }
+
+                push( @table, \@row );
             }
-            my $target = $piece->{'ItemStats'}{$useStat}{'NQ'};
-            my $delta  = $target - $useValue;
-            my $rate
-                = $delta != 0
-                ? sprintf( '%.02f', $piece->{'CountCost'} / $delta )
-                : 0;
 
-            push( @row, $useValue, $delta, $rate );
+            message( $obj->{'Name'} );
+            @table = sort {
+                return 1                   if( $a->[5] <= 0 && $b->[5] > 0 );
+                return -1                  if( $b->[5] <= 0 && $a->[5] > 0 );
+                return $b->[5] <=> $a->[5] if( $a->[5] <= 0 && $b->[5] <= 0 );
+                $a->[5] <=> $b->[5];
+            } @table;
 
-            push( @row, $piece->{'ItemReceive'}, $piece->{'CountCost'} );
-            #            foreach my $stat ( @{ $config{$job}{stats} } ) {
-            my $value = $piece->{'ItemStats'}{$useStat}{'NQ'};
-            push( @row, $value );
-            #            }
+            my $need = 0;
+            my $cost = 0;
+            foreach my $row (@table) {
+                if( $row->[5] <= 0 ) {
+                    $_ = green($_) foreach @{$row};
+                } else {
+                    $need++;
+                    $cost += $row->[7];
+                }
+            }
 
-            push( @table, \@row );
-        }
-
-        message( $obj->{'Name'} );
-        @table = sort {
-            return 1                   if( $a->[5] <= 0 && $b->[5] > 0 );
-            return -1                  if( $b->[5] <= 0 && $a->[5] > 0 );
-            return $b->[5] <=> $a->[5] if( $a->[5] <= 0 && $b->[5] <= 0 );
-            $a->[5] <=> $b->[5];
-        } @table;
-
-        my $need = 0;
-        my $cost = 0;
-        foreach my $row (@table) {
-            if( $row->[5] <= 0 ) {
-                $_ = green($_) foreach @{$row};
-            } else {
-                $need++;
-                $cost += $row->[7];
+            if( $need > 0 ) {
+                dump_table(
+                    table => [
+                        [   qw( slot id name current delta rate item cost value )
+                        ],
+                        @table
+                    ]
+                );
+                say("$need items to purchace");
+                say("$cost total tomestones");
             }
         }
-
-        if( $need > 0 ) {
-            dump_table(
-                table => [
-                    [qw( slot id name current delta rate item cost value )],
-                    @table
-                ]
-            );
-            say("$need items to purchace");
-            say("$cost total tomestones");
-        }
+        $lower = $shopLevel;
     }
 
 }
@@ -223,7 +200,7 @@ sub getCharacterSheet {
 
     my $retval = undef;
     my $job    = $opts{'job'};
-    $job = uc($job) if( $job );
+    $job = uc($job) if($job);
     if($job) {
         $retval = $api->cached("character.$opts{'lodestone'}.$job");
         if($retval) {
